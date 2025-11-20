cd16755ab0e3702d28ab92364b17438bff5c2030
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-11-20 13:27:58 +0700

    feat: define encoder interface

---
+++ internal/qrencode/encoder.go
@@ -0,0 +1,28 @@
+package qrencode
+
+type Encoder interface {
+	Encode() []string
+}
+
+func NewEncoder(s string) Encoder {
+	encodingMode := DetermineEncodingMode(s)
+
+	switch encodingMode {
+	case NumericMode:
+		return NewNumericEncoder(s)
+
+		// case AlphanumericMode:
+		// 	return NewAlphanumericEncoder(s)
+
+		// case KanjiMode:
+		// 	return NewKanjiEncoder(s)
+
+		// case ByteMode:
+		// 	return NewByteEncoder(s)
+
+		// case ECIMode:
+		// 	return NewECIEncoder(s)
+	}
+
+	return nil
+}
