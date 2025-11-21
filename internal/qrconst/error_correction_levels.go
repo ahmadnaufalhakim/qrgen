3943dd6bcc59bbcdc24c29a7f147e7e7ec99a958
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-11-21 20:57:47 +0700

    chore: add ErrorCorrectionLevel type

---
+++ internal/qrconst/error_correction_levels.go
@@ -0,0 +1,10 @@
+package qrconst
+
+type ErrorCorrectionLevel rune
+
+const (
+	L ErrorCorrectionLevel = 'L'
+	M ErrorCorrectionLevel = 'M'
+	Q ErrorCorrectionLevel = 'Q'
+	H ErrorCorrectionLevel = 'H'
+)
