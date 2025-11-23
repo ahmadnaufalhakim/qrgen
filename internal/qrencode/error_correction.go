ab27f4e88cb7a88d4a789515112d9fa10a108a25
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-11-23 22:18:48 +0700

    feat: add GF256 functions (add and mul), GeneratorPolynomial and PolynomialsProduct functions for error correction

--- internal/qrencode/error_correction.go
+++ internal/qrencode/error_correction.go
@@ -1,6 +1,8 @@
 package qrencode
 
 import (
+	"fmt"
+
 	"github.com/ahmadnaufalhakim/qrgen/internal/qrconst"
 	"github.com/ahmadnaufalhakim/qrgen/internal/tables"
 )
@@ -36,4 +38,76 @@ func GroupDataCodewords(
 	}
 
 	return groups, nil
+}
+
+func GeneratorPolynomial(n int) ([]uint8, error) {
+	if n <= 0 {
+		return nil, fmt.Errorf("n must be positive")
+	}
+
+	if g, ok := tables.GeneratorPolynomial[n]; ok {
+		return g, nil
+	}
+
+	var polynomials [][]uint8
+	for i := n; i >= 0; i-- {
+		if g, ok := tables.GeneratorPolynomial[i]; ok {
+			polynomials = append(polynomials, g)
+			break
+		} else {
+			polynomials = append(polynomials, []uint8{1, tables.AntilogGF256[i]})
+		}
+	}
+
+	prod, err := PolynomialsProduct(polynomials...)
+	if err != nil {
+		return nil, err
+	}
+
+	tables.GeneratorPolynomial[n] = prod
+
+	return prod, nil
+}
+
+func PolynomialsProduct(polynomials ...[]uint8) ([]uint8, error) {
+	if len(polynomials) == 0 {
+		return nil, fmt.Errorf("must provide at least 1 polynomial")
+	}
+
+	prod := []uint8{1}
+	for _, p := range polynomials {
+		prod = multiplyTwoPolynomials(prod, p)
+	}
+
+	return prod, nil
+}
+
+func multiplyTwoPolynomials(a, b []uint8) []uint8 {
+	m := len(a)
+	n := len(b)
+
+	result := make([]uint8, m+n-1)
+
+	for i := range m {
+		for j := range n {
+			result[i+j] = addGF256(result[i+j], mulGF256(a[i], b[j]))
+		}
+	}
+
+	return result
+}
+
+func addGF256(x, y uint8) uint8 {
+	return x ^ y
+}
+
+func mulGF256(x, y uint8) uint8 {
+	if x == 0 || y == 0 {
+		return 0
+	}
+
+	logX := uint16(tables.LogGF256[x])
+	logY := uint16(tables.LogGF256[y])
+
+	return tables.AntilogGF256[(logX+logY)%255]
 }
