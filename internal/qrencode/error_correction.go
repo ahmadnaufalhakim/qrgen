d79419fc5aff69bf9db365623e995115cc8d3f4f
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-11-24 18:28:23 +0700

    feat: add MessagePolynomial function, bit string to byte helper func, polynomial addition and division, and discard leading zeros helper func

--- internal/qrencode/error_correction.go
+++ internal/qrencode/error_correction.go
@@ -40,6 +40,19 @@ func GroupDataCodewords(
 	return groups, nil
 }
 
+func MessagePolynomial(block []string) ([]uint8, error) {
+	var m []uint8
+	for _, dataCodeword := range block {
+		b, err := bitStringToByte(dataCodeword)
+		if err != nil {
+			return nil, err
+		}
+		m = append(m, b)
+	}
+
+	return m, nil
+}
+
 func GeneratorPolynomial(n int) ([]uint8, error) {
 	if n <= 0 {
 		return nil, fmt.Errorf("n must be positive")
@@ -82,6 +95,48 @@ func PolynomialsProduct(polynomials ...[]uint8) ([]uint8, error) {
 	return prod, nil
 }
 
+func bitStringToByte(s string) (uint8, error) {
+	if len(s) != 8 {
+		return 0, fmt.Errorf("must be 8 bits")
+	}
+
+	b := uint8(0)
+	for i := range 8 {
+		b <<= 1
+		if s[i] == '1' {
+			b |= 1
+		} else if s[i] != '0' {
+			return 0, fmt.Errorf("invalid bit")
+		}
+	}
+
+	return b, nil
+}
+
+func addTwoPolynomials(a, b []uint8) []uint8 {
+	m := len(a)
+	n := len(b)
+	maxDegree := max(m, n)
+
+	result := make([]uint8, maxDegree)
+
+	for i := range maxDegree {
+		x := uint8(0)
+		if i < m {
+			x = a[m-i-1]
+		}
+
+		y := uint8(0)
+		if i < n {
+			y = b[n-i-1]
+		}
+
+		result[maxDegree-i-1] = addGF256(x, y)
+	}
+
+	return result
+}
+
 func multiplyTwoPolynomials(a, b []uint8) []uint8 {
 	m := len(a)
 	n := len(b)
@@ -95,6 +150,46 @@ func multiplyTwoPolynomials(a, b []uint8) []uint8 {
 	}
 
 	return result
+}
+
+func divideTwoPolynomials(a, b []uint8) []uint8 {
+	m := len(a)
+	n := len(b)
+
+	a = append(a, make([]uint8, n-1)...)
+	b = append(b, make([]uint8, m-1)...)
+
+	dividend := a
+	divisor := make([]uint8, len(b))
+	copy(divisor, b)
+
+	for i := range m {
+		aLeadCoefExp := tables.LogGF256[dividend[0]]
+		bLeadCoefExp := tables.LogGF256[b[0]]
+
+		multiplierExp := 255 + uint16(aLeadCoefExp) - uint16(bLeadCoefExp)
+		for j := range n {
+			divisor[j] = mulGF256(divisor[j], tables.AntilogGF256[multiplierExp%255])
+		}
+
+		dividend = addTwoPolynomials(dividend, divisor)
+		dividend = discardLeadingZeros(dividend)
+
+		divisor = make([]uint8, len(b)-i-1)
+		copy(divisor, b[:len(b)-i-1])
+	}
+
+	return dividend
+}
+
+func discardLeadingZeros(polynomial []uint8) []uint8 {
+	for i, coef := range polynomial {
+		if coef != 0 {
+			return polynomial[i:]
+		}
+	}
+
+	return []uint8{}
 }
 
 func addGF256(x, y uint8) uint8 {
