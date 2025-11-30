d4088640bbd21d0b7ba768826242c2823dd824ac
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-11-30 17:30:58 +0700

    feat: further restrict characters that are encodable by Kanji mode to *kanji only* characters

--- internal/encoder/encoder.go
+++ internal/encoder/encoder.go
@@ -38,12 +38,12 @@ func NewEncoder(s string) Encoder {
 func determineEncodingMode(s string) qrconst.EncodingMode {
 	isNumeric := true
 	isAlphanumeric := true
-	isShiftJIS := true
+	isKanji := true
 
 	sjisEncoder := japanese.ShiftJIS.NewEncoder()
 
 	for _, r := range s {
-		if isNumeric || isAlphanumeric || isShiftJIS {
+		if isNumeric || isAlphanumeric || isKanji {
 			if isNumeric {
 				if r < '0' || r > '9' {
 					isNumeric = false
@@ -56,17 +56,41 @@ func determineEncodingMode(s string) qrconst.EncodingMode {
 				}
 			}
 
-			if isShiftJIS {
+			if isKanji {
 				sjisBytes, err := sjisEncoder.Bytes([]byte(string(r)))
 
 				if err != nil || len(sjisBytes) != 2 {
-					isShiftJIS = false
+					isKanji = false
 				} else {
 					sjisValue := (uint16(sjisBytes[0]) << 8) | uint16(sjisBytes[1])
 					if !(sjisValue >= 0x8140 && sjisValue <= 0x9FFC) &&
 						!(sjisValue >= 0xE040 && sjisValue <= 0xEBBF) {
-						isShiftJIS = false
+						isKanji = false
+						continue
 					}
+
+					// Convert Shift-JIS -> JIS X 0208 row/column (QR Spec)
+					var row, col int
+
+					// Row
+					if sjisBytes[0] <= 0x9F {
+						row = int(sjisBytes[0]-0x81)*2 + 1
+					} else {
+						row = int(sjisBytes[0]-0xC1)*2 + 1
+					}
+					if sjisBytes[1] >= 0x9F {
+						row++
+					}
+
+					// Column
+					if sjisBytes[1] >= 0x9F {
+						col = int(sjisBytes[1] - 0x7E)
+					} else {
+						col = int(sjisBytes[1] - 0x40)
+					}
+
+					// QR in Kanji mode uses JIS X 0208 rows/cols 1..94
+					isKanji = (row >= 1 && row <= 94) && (col >= 1 && col <= 94)
 				}
 			}
 		} else {
@@ -80,7 +104,7 @@ func determineEncodingMode(s string) qrconst.EncodingMode {
 	if isAlphanumeric {
 		return qrconst.AlphanumericMode
 	}
-	if isShiftJIS {
+	if isKanji {
 		return qrconst.KanjiMode
 	}
 	return qrconst.ByteMode
