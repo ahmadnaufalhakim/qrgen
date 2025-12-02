e1d171b6e76513f436dc12d4d0190a0062e4a57e
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-02 23:36:07 +0700

    feat: add bit string padding helper function in `qrencode` package

--- internal/qrencode/bit.go
+++ internal/qrencode/bit.go
@@ -33,3 +33,18 @@ func byteToBitString(b uint8) string {
 
 	return string(buf[:])
 }
+
+func padBitString(s string, width int) string {
+	if len(s) >= width {
+		return s
+	}
+
+	pad := width - len(s)
+	buf := make([]byte, width)
+	for i := range pad {
+		buf[i] = '0'
+	}
+	copy(buf[pad:], s)
+
+	return string(buf)
+}
