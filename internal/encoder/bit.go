098c3479ee19c7d043a367c5e061b5020a5155e6
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-02 23:24:55 +0700

    feat: add bit helper function in encoder

---
+++ internal/encoder/bit.go
@@ -0,0 +1,16 @@
+package encoder
+
+func padBitString(s string, width int) string {
+	if len(s) > width {
+		return s
+	}
+
+	pad := width - len(s)
+	buf := make([]byte, width)
+	for i := range pad {
+		buf[i] = '0'
+	}
+	copy(buf[pad:], s)
+
+	return string(buf)
+}
