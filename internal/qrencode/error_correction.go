45cb2dc0231405c218a8ef034d3a90ad76d0fddc
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-06 14:40:29 +0700

    chore: move and rename the StructureFinalMessage to InterleaveBlocks in `error_correction.go`,

--- internal/qrencode/error_correction.go
+++ internal/qrencode/error_correction.go
@@ -2,6 +2,7 @@ package qrencode
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/ahmadnaufalhakim/qrgen/internal/qrconst"
 	"github.com/ahmadnaufalhakim/qrgen/internal/tables"
@@ -135,4 +136,86 @@ func GenerateErrorCorrectionBlocks(
 	}
 
 	return ecBlocks, nil
+}
+
+// InterleaveBlocks interleaves the data and error correction codewords
+// according to the QR Code specification and returns the final message
+// bit string representing the full message ready for placement in the
+// QR matrix.
+//
+// It handles both group 1 and group 2 blocks, performs column-wise
+// interleaving, appends the error correction codewords, and adds the
+// remainder bits for the given QR version.
+func InterleaveBlocks(
+	version int,
+	ecLevel qrconst.ErrorCorrectionLevel,
+	dataBlocks [][]string,
+	ecBlocks [][]uint8,
+) (string, error) {
+	if len(dataBlocks) != len(ecBlocks) {
+		return "", fmt.Errorf("number of data blocks must be equal to the number of error correction blocks")
+	}
+
+	ecBlockInfo := tables.ECBlockInfos[ecLevel][version-1]
+	ecCodewordsPerBlock := ecBlockInfo.ECCodewordsPerBlock
+	group1Blocks := ecBlockInfo.Group1Blocks
+	group2Blocks := ecBlockInfo.Group2Blocks
+	group1DataCodewordsPerBlock := ecBlockInfo.Group1DataCodewordsPerBlock
+	group2DataCodewordsPerBlock := ecBlockInfo.Group2DataCodewordsPerBlock
+	group1DataCodewords := group1Blocks * group1DataCodewordsPerBlock
+	group2DataCodewords := group2Blocks * group2DataCodewordsPerBlock
+
+	// Interleave the data codewords
+	totalDataCodewords := group1DataCodewords + group2DataCodewords
+	interleavedDataCodewords := make([]uint8, totalDataCodewords)
+	dataCols := max(group1DataCodewordsPerBlock, group2DataCodewordsPerBlock)
+	group1Offset := group1Blocks
+	for j := range dataCols {
+		if j < group1DataCodewordsPerBlock {
+			for i := range group1Blocks {
+				b, err := bitStringToByte(dataBlocks[i][j])
+				if err != nil {
+					return "", err
+				}
+
+				interleavedDataCodewords[(group1Blocks+group2Blocks)*j+i] = b
+			}
+			group1Offset = group1Blocks
+		} else {
+			group1Offset = 0
+		}
+
+		if j < group2DataCodewordsPerBlock {
+			for i := range group2Blocks {
+				b, err := bitStringToByte(dataBlocks[group1Blocks+i][j])
+				if err != nil {
+					return "", err
+				}
+
+				interleavedDataCodewords[(group1Blocks+group2Blocks)*j+group1Offset+i] = b
+			}
+		}
+	}
+
+	// Interleave the error correction codewords
+	totalECCodewords := (group1Blocks + group2Blocks) * ecCodewordsPerBlock
+	interleavedECCodewords := make([]uint8, totalECCodewords)
+	ecCols := ecCodewordsPerBlock
+	for j := range ecCols {
+		for i := range group1Blocks + group2Blocks {
+			interleavedECCodewords[(group1Blocks+group2Blocks)*j+i] = ecBlocks[i][j]
+		}
+	}
+
+	// Build the final message string
+	var finalMessageBuilder strings.Builder
+	for _, dataCodeword := range interleavedDataCodewords {
+		finalMessageBuilder.WriteString(byteToBitString(dataCodeword))
+	}
+	for _, ecCodeword := range interleavedECCodewords {
+		finalMessageBuilder.WriteString(byteToBitString(ecCodeword))
+	}
+	finalMessageBuilder.WriteString(strings.Repeat("0", tables.RemainderBits[version-1]))
+
+	return finalMessageBuilder.String(), nil
 }
